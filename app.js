f14adf9651c77a693435cd2babeb83bfce87cedc
LenOk <95621680+wee-owl@users.noreply.github.com>
2022-10-26 13:20:14 +0300

    refactor: add alt to media content according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt="Button delete";
     deleteButton.appendChild(deleteButtonImg);
 
 
